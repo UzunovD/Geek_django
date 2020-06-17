e417d2fdff3552194f95e33142b4365db38dfd85
Dmitry Uzunov <duyz@ya.ru>
2020-06-17 13:32:42 +0400

    add static

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,11 +4,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'mainapp/index.html')
 
+
 def contact(request):
     return render(request, 'mainapp/contact.html')
 
+
 def product(request):
     return render(request, 'mainapp/product.html')
+
 
 def product_deails(request):
     return render(request, 'mainapp/product-deails.html')
