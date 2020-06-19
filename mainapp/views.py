28c32a4e8c620d5a9ab7f07395a1938a17389827
Dmitry Uzunov <duyz@ya.ru>
2020-06-19 17:46:17 +0400

    lesson 2:   - templates inheritance product-deails.html   - static tag   - url tag   - fot tag   - if tag   - dz

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 def index(request):
     context = {
         'page_title': 'shop',
+        'active': 'menu_link_active',
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -15,23 +16,24 @@ def contact(request):
             'phone': '1900 1234 5678 ',
             'email': 'info@interior.com',
             'address': '12 W 1st St, 90001 Los Angeles, California',
-         },
+        },
         {
             'loc': 'Texas',
             'phone': '1900 1234 5678 ',
             'email': 'info@interior.com',
             'address': '12 W 1st St, 90001 New Jersia, Texas',
-         },
+        },
         {
             'loc': 'Man',
             'phone': '1900 1234 5678 ',
             'email': 'info@interior.com',
             'address': '12 W 1st St, 90001 Saint Men, Man',
-         },
+        },
     ]
     context = {
         'page_title': 'contact',
-         'locations': locations,
+        'locations': locations,
+        'active_CONTACT': 'menu_link_active',
     }
     return render(request, 'mainapp/contact.html', context)
 
@@ -39,12 +41,14 @@ def contact(request):
 def products(request):
     context = {
         'page_title': 'products',
+        'active_PRODUCTS': 'menu_link_active',
     }
     return render(request, 'mainapp/products.html', context)
 
 
 def product_details(request):
     context = {
-        'page_title': 'Product deails',
+        'page_title': 'product deails',
+        'active_SHOWROOM': 'menu_link_active',
     }
     return render(request, 'mainapp/product_details.html', context)
