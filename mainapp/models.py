b35cff5956e1f465f2329d5251af5ed848652b7f
Dmitry Uzunov <duyz@ya.ru>
2020-06-21 22:23:28 +0400

    lesson 3:   - models*

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField('category name', max_length=50)
+    description = models.TextField('category description', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField('product name', max_length=100)
+    category = models.ForeignKey(ProductCategory, models.CASCADE, verbose_name='category of product')
+    description = models.TextField('product description', blank=True)
+    short_desc = models.CharField('short descriotion', max_length=50, blank=True)
+    price = models.DecimalField('Price', max_digits=8, decimal_places=2, default=0)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    quantity = models.PositiveIntegerField('quantity', default=0)
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 def index(request):
     context = {
         'page_title': 'shop',
-        'active': 'menu_link_active',
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -33,7 +32,6 @@ def contact(request):
     context = {
         'page_title': 'contact',
         'locations': locations,
-        'active_CONTACT': 'menu_link_active',
     }
     return render(request, 'mainapp/contact.html', context)
 
@@ -41,7 +39,6 @@ def contact(request):
 def products(request):
     context = {
         'page_title': 'products',
-        'active_PRODUCTS': 'menu_link_active',
     }
     return render(request, 'mainapp/products.html', context)
 
@@ -49,6 +46,5 @@ def products(request):
 def product_details(request):
     context = {
         'page_title': 'product deails',
-        'active_SHOWROOM': 'menu_link_active',
     }
     return render(request, 'mainapp/product_details.html', context)
