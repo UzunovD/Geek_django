f141f851016b3cea0a0da77787d881c3c2d64e61
Dmitry Uzunov <duyz@ya.ru>
2020-06-22 21:00:43 +0400

    lesson 3:   - dz

--- mainapp/models.py
+++ mainapp/models.py
@@ -12,6 +12,7 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField('product name', max_length=100)
     category = models.ForeignKey(ProductCategory, models.CASCADE, verbose_name='category of product')
+    type_prod = models.CharField('type of product', max_length=50, blank=True, null=True)
     description = models.TextField('product description', blank=True)
     short_desc = models.CharField('short descriotion', max_length=50, blank=True)
     price = models.DecimalField('Price', max_digits=8, decimal_places=2, default=0)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from mainapp.models import ProductCategory, Product
+
 
 def index(request):
+    products4 = Product.objects.all()[:4]
+    products6 = Product.objects.all()[4:10]
     context = {
         'page_title': 'shop',
+        'products4': products4,
+        'products6': products6,
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -37,14 +43,26 @@ def contact(request):
 
 
 def products(request):
+    categories = ProductCategory.objects.all()
+    products = Product.objects.all()
+
     context = {
         'page_title': 'products',
+        'categories': categories,
+        'products': products,
     }
     return render(request, 'mainapp/products.html', context)
 
 
 def product_details(request):
+    categories = ProductCategory.objects.all()
+    products = Product.objects.all()
+    products3 = Product.objects.all()[4:7]
+
     context = {
         'page_title': 'product deails',
+        'categories': categories,
+        'products': products,
+        'products3': products3,
     }
     return render(request, 'mainapp/product_details.html', context)
