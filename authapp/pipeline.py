63104cf0458e10d7088627f94ac20ae10f55d950
Dmitry Uzunov <duyz@ya.ru>
2020-07-20 14:57:14 +0400

    Django2 dz2:     - social auth(google+)     - ShopUserProfile (extended the table ShopUser)     - signals     - ShopUserProfile (updating)     - social auth extended

---
+++ authapp/pipeline.py
@@ -0,0 +1,26 @@
+from social_core.exceptions import AuthForbidden
+
+from authapp.models import ShopUserProfile
+
+
+def fill_from_social_user_profile(backend, user, response, *args, **kwargs):
+    print(response)
+    if backend.name == 'google-oauth2':
+        if 'gender' in response.keys():
+            if response['gender'] == 'male':
+                user.shopuserprofile.gender = ShopUserProfile.FAMALE
+            elif response['gender'] == 'man':
+                user.shopuserprofile.gender = ShopUserProfile.MALE
+            else:
+                user.shopuserprofile.gender = ShopUserProfile.ANOTHER
+        if 'tagline' in response.keys():
+            user.shopuserprofile.tagline = response['tagline']
+        if 'aboutMe' in response.keys():
+            user.shopuserprofile.aboutMe = response['aboutMe']
+        if 'ageRange' in response.keys():
+            minAge = response['ageRange']['min']
+            if int(minAge) < 18:
+                user.delete()
+                raise AuthForbidden('social_core.backends.google.GoogleOAuth2')
+
+        user.save()
