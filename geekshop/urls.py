e6954404f6ec62041b68d050396e28297571dcda
Dmitry Uzunov <duyz@ya.ru>
2020-08-02 20:05:22 +0400

    Django2 dz6:     - add postgres bd for prod server     - add debug toolbar

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf import settings
 
@@ -32,4 +32,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
