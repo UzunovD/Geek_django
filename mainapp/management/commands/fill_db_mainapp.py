3deaeca9d767c8e708100d2d87b2c6ff6794311c
Dmitry Uzunov <duyz@ya.ru>
2020-08-10 17:36:19 +0400

    Django2 dz8:     - bulk_create     - add create superuser

--- mainapp/management/commands/fill_db_mainapp.py
+++ mainapp/management/commands/fill_db_mainapp.py
@@ -4,6 +4,8 @@ import os
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+
+from authapp.models import ShopUser
 from mainapp.models import ProductCategory, Product
 
 
@@ -20,13 +22,29 @@ class Command(BaseCommand):
         categories = load_from_json('categories')
 
         ProductCategory.objects.all().delete()
-        [ProductCategory.objects.create(**category) for category in categories]
+        # [ProductCategory.objects.create(**category) for category in categories]
+        product_category_obj = [ProductCategory(**category) for category in \
+                categories]
+        ProductCategory.objects.bulk_create(product_category_obj)
 
         products = load_from_json('products')
         Product.objects.all().delete()
+        product_obj = []
+        chunk_size = 4000
         for product in products:
             category_name = product['category']
-
-            _category = ProductCategory.objects.filter(name=category_name).first()
+            _category = ProductCategory.objects.filter(
+                name=category_name).first()
             product['category'] = _category
-            Product.objects.create(**product)
+            product_obj.append(Product(**product))
+            if len(product_obj) == chunk_size:
+                Product.objects.bulk_create(product_obj)
+                product_obj = []
+        if product_obj:
+            Product.objects.bulk_create(product_obj)
+
+        if not ShopUser.objects.filter(username='django').exists():
+            ShopUser.objects.create_superuser(username='django',
+                                              email='admin@geekshop',
+                                              password='geekbrains')
+
